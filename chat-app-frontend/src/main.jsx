69bf687a16bd015da26efad01fc1719575d41ad7
samikshathorat21 <thoratsamiksha19@gmail.com>
2026-03-24 11:00:20 +0530

    Deployed Frontend

--- chat-app-frontend/src/main.jsx
+++ chat-app-frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { Toaster } from "react-hot-toast";
 import { BrowserRouter } from "react-router";
-import AppRoutes from "./config/routes.jsx";
+import AppRoutes from "./config/Routes.jsx";
 import { ChatProvider } from "./context/ChatContext.jsx";
 import "./index.css";
 
