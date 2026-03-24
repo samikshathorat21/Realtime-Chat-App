9af3b946e9b88d4b9d4d275fc0443f7e4dac631b
samikshathorat21 <thoratsamiksha19@gmail.com>
2026-03-24 10:51:40 +0530

    Deployed Frontend

--- chat-app-frontend/src/config/AxiosHelper.js
+++ chat-app-frontend/src/config/AxiosHelper.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const baseURL = "http://localhost:8080";
+export const baseURL = "https://realtime-chat-app-2-zszw.onrender.com";
 export const httpClient = axios.create({
   baseURL: baseURL,
 });
